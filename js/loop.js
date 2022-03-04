ecf4e24971bd16f24f56ded09df2cf8a47efc459
ProjectDragonflyRIS <100748990+ProjectDragonflyRIS@users.noreply.github.com>
2022-03-04 23:08:11 +0400

    Add files via upload

--- js/loop.js
+++ js/loop.js
@@ -3,21 +3,56 @@ function runLoop() {
 
     hideFooter()
 
+    adjustNavbar()
+
     // run again after 100ms
     setTimeout(runLoop, 100)
 }
 
 // hide the footer if its at the bottom of the page
 function hideFooter() {
-    // check if scrolled to the bottom of the page
-    if ((Math.ceil(window.innerHeight) + Math.ceil(window.scrollY)) >= Math.ceil(document.body.scrollHeight)) {
-        // play hide animation and hide the footer
-        document.getElementById("footer").style.top = "100vw";
-        document.getElementById("footer").style.animationName = "footer-slide-out";
+    // only hide footer if on desktop
+    if (!(window.innerHeight > window.innerWidth)) {
+        // check if scrolled to the bottom of the page
+        if ((Math.ceil(window.innerHeight) + Math.ceil(window.scrollY)) >= Math.ceil(document.body.scrollHeight)) {
+            // play hide animation and hide the footer
+            document.getElementById("footer").style.top = "100vw";
+            document.getElementById("footer").style.animationName = "footer-slide-out";
+        } else {
+            // show the footer
+            document.getElementById("footer").style.top = "calc(100vh - 80px)";
+            document.getElementById("footer").style.animationName = "footer-slide-in";
+        }
+    }
+}
+
+/* always adjust the navigation bar according to the orientation of the screen */
+function adjustNavbar() {
+    navbar = document.getElementById('navbar');
+    if (opened == false) {
+        // check if in portrait mode and change navbar accordingly
+        if (window.innerHeight > window.innerWidth) {
+            navbar.style.top = '-80vh';
+            navbar.style.left = '0px';
+            navbar.style.width = '100vw';
+            navbar.style.height = '80vh';
+        } else {
+            navbar.style.left = '-250px';
+            navbar.style.top = '0px';
+            navbar.style.width = '16vw';
+            navbar.style.height = '100vh';
+        }
     } else {
-        // show the footer
-        document.getElementById("footer").style.top = "calc(100vh - 80px)";
-        document.getElementById("footer").style.animationName = "footer-slide-in";
+        navbar.style.top = '0px';
+        navbar.style.left = '0px';
+        // check if in portrait mode and change navbar accordingly
+        if (window.innerHeight > window.innerWidth) {
+            navbar.style.width = '100vw';
+            navbar.style.height = '80vh';
+        } else {
+            navbar.style.width = '16vw';
+            navbar.style.height = '100vh';
+        }
     }
 }
 
--- js/menu.js
+++ js/menu.js
@@ -5,15 +5,27 @@ function menuPressed() {
     navbar = document.getElementById('navbar');
     menuBG = document.getElementById('menu-background');
     if (opened == true) {
-        navbar.style.left = '-250px';
-        navbar.style.animationName = 'menu-slide-out';
+        // check if in portrait mode and change navbar accordingly
+        if (window.innerHeight > window.innerWidth) {
+            navbar.style.top = '-80vh';
+            navbar.style.animationName = 'navbar-slide-out-mobile';
+        } else {
+            navbar.style.left = '-250px';
+            navbar.style.animationName = 'navbar-slide-out-desktop';
+        }
         menuBG.style.backgroundColor = 'transparent';
         menuBG.style.width = '0px'
         menuBG.style.animationName = 'menu-background-off';
         opened = false;
     } else {
-        navbar.style.left = '0px';
-        navbar.style.animationName = 'menu-slide-in';
+        // check if in portrait mode and change navbar accordingly
+        if (window.innerHeight > window.innerWidth) {
+            navbar.style.top = '0px';
+            navbar.style.animationName = 'navbar-slide-in-mobile';
+        } else {
+            navbar.style.left = '0px';
+            navbar.style.animationName = 'navbar-slide-in-desktop';
+        }
         menuBG.style.backgroundColor = 'rgba(255, 255, 255, 0.25)';
         menuBG.style.width = '100vw'
         menuBG.style.animationName = 'menu-background-on';
